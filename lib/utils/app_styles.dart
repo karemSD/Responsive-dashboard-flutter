98aefbfa98ba5cd302d039a3d90a111575a5208d
karem sd <saad.karem8642@gmail.com>
2024-03-31 02:48:34 +0300

    Add new images and fonts to assets
    Added new images to assets/images/ and included a new font file
    (Montserrat-VariableFont_wght.ttf) in the assets. Updated pubspec.yaml
    to include these new assets and fonts. Created a new file, app_images.dart,
    to reference the added images in the assets.

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -3,64 +3,64 @@ import 'package:flutter/material.dart';
 abstract class AppStyles {
   static const TextStyle styleRegular16 = TextStyle(
     color: Color(0XFF064060),
-    fontFamily: "montserrat",
+    fontFamily: "Montserrat",
     fontSize: 16,
     fontWeight: FontWeight.w400,
   );
 
   static const TextStyle styleMedium16 = TextStyle(
     color: Color(0XFF064060),
-    fontFamily: "montserrat",
+    fontFamily: "Montserrat",
     fontSize: 16,
     fontWeight: FontWeight.w500,
   );
   static const TextStyle styleMedium20 = TextStyle(
     color: Color(0XFFFFFFFF),
-    fontFamily: "montserrat",
+    fontFamily: "Montserrat",
     fontSize: 20,
     fontWeight: FontWeight.w500,
   );
 
   static const TextStyle styleSemiBold16 = TextStyle(
     color: Color(0XFF064060),
-    fontFamily: "montserrat",
+    fontFamily: "Montserrat",
     fontSize: 16,
     fontWeight: FontWeight.w600,
   );
 
   static const TextStyle styleSemiBold20 = TextStyle(
     color: Color(0XFF064060),
-    fontFamily: "montserrat",
+    fontFamily: "Montserrat",
     fontSize: 20,
     fontWeight: FontWeight.w600,
   );
   static const TextStyle styleRegular12 = TextStyle(
     color: Color(0XFFAAAAAA),
-    fontFamily: "montserrat",
+    fontFamily: "Montserrat",
     fontSize: 12,
     fontWeight: FontWeight.w400,
   );
   static const TextStyle styleSemiBold24 = TextStyle(
     color: Color(0XFF4EB7F2),
-    fontFamily: "montserrat",
+    fontFamily: "Montserrat",
     fontSize: 24,
     fontWeight: FontWeight.w600,
   );
   static const TextStyle styleBold16 = TextStyle(
     color: Color(0XFF4EB7F2),
-    fontFamily: "montserrat",
+    fontFamily: "Montserrat",
     fontSize: 16,
     fontWeight: FontWeight.w700,
   );
   static const TextStyle styleSemiBold18 = TextStyle(
     color: Color(0XFFFFFFFF),
-    fontFamily: "montserrat",
+    fontFamily: "Montserrat",
     fontSize: 18,
     fontWeight: FontWeight.w600,
   );
   static const TextStyle styleRegular14 = TextStyle(
     color: Color(0XFFAAAAAA),
-    fontFamily: "montserrat",
+    fontFamily: "Montserrat",
     fontSize: 14,
     fontWeight: FontWeight.w400,
   );
